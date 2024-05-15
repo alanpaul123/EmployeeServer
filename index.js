bc38fa601de11613c93aad492daeb74119aabdd2
alanpaul123 <alanpauldxb@gmail.com>
2024-05-15 18:16:43 +0400

    done

---
+++ index.js
@@ -0,0 +1,20 @@
+const jsonServer = require("json-server");
+// Create a server for Employee
+const EmpServer = jsonServer.create();
+// create middleware
+const middleware = jsonServer.defaults();
+
+// Define route for json file in server
+
+const route = jsonServer.router("db.json");
+
+//set up port for running server app
+const PORT = 3000 || process.env.PORT;
+
+EmpServer.use(middleware);
+EmpServer.use(route);
+EmpServer.listen(PORT, () => {
+  console.log(
+    `MEdia player server started at port ${PORT} and waiting for client request`
+  );
+});
